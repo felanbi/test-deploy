1803497436af9af78de8a9624158f6503b402f37
Fayssal <elanbifayssal@gmail.com>
2024-11-09 16:38:09 +0100

    add ruff pre commit hooks

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,13 +2,15 @@ import pytest
 from flask.testing import FlaskClient
 from app import app
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+
 def test_hello_world(client: FlaskClient) -> str:
     response = client.get("/")
-    
+
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
